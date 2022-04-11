e626e5c27c69638624ead3700330cfc79b36d4fb
galvan29 <matteogalvan1999@gmail.com>
2022-04-11 18:45:53 +0200

    dsadasd

--- matrici/3.hs
+++ matrici/3.hs
@@ -1,6 +1,6 @@
 {- let y:ys = x in ciao y:ys -} 
 main = do 
-    print (map strangeSum (colaltsums2 [[1,2,3],[4,5,6],[7,8,9]]))
+    print (colaltsums [[1,2,3],[4,5,6],[7,8,9]])
 
 colaltsums :: [[Integer]] -> [Integer]
 colaltsums (x:xs) = map strangeSum (colaltsums2 (x:xs))
---
+++ matrici/4.hs
@@ -0,0 +1,32 @@
+{- 4. Si scriva una funzione colMinMax che, data una matrice implementata come liste di liste per righe,
+calcola il vettore delle coppie (minimo,massimo) delle colonne della matrice. -}
+
+main = do 
+    print (colMinMax [[1,2,3],[4,5,6],[7,8,9]])
+
+colMinMax :: [[Integer]] -> [Integer]
+colMinMax (x:xs) = (map (minore 10) (colsums (x:xs)))
+{- (head (map (minore 10) (colsums (x:xs))), head (map (maggiore 0) (colsums (x:xs)))) -}
+minore :: Integer -> [Integer] -> Integer   {- metto uno dei due -}
+minore p [] = p
+minore p (x:xs)
+   | x <= p = minore x (xs)
+   | x > p = minore p (xs) 
+
+maggiore :: Integer -> [Integer] -> Integer   {- metto uno dei due -}
+maggiore p [] = p
+maggiore p (x:xs)
+   | x > p = maggiore x (xs)
+   | x <= p = maggiore p (xs) 
+
+
+
+
+
+colsums :: [[Integer]] -> [[Integer]]
+colsums ([]:_) = []
+colsums (x:xs) = work (x:xs) : (colsums (map tail (x:xs)))
+
+work :: [[Integer]] -> [Integer]
+work [] = []
+work (x:xs) = let (f:fs) = x in f : work xs
