89248244a17a42461e71b1b5e5dad9a97f981c93
galvan29 <matteogalvan1999@gmail.com>
2022-04-12 00:17:24 +0200

    dd

--- matrici/6.hs
+++ matrici/6.hs
@@ -5,13 +5,25 @@
 main = do
   print (low [[1,2,3],[4,5,6],[8,9,0]])
   print (low [[1,0,0],[2,-3,0],[4,5,6]])
+  print (low [[1,2,1],[0,-3,2],[0,0,6]])
   print (check [1,0,0])
   print (check [0,0,0])
 
 low :: [[Integer]] -> Bool
-low [] = True
-low (x:xs)
-   | (check (tail aa)) == True = low (remCl xs)
+low (x:xs) = low2 (colsums (x:xs))
+
+colsums :: [[Integer]] -> [[Integer]]
+colsums ([]:_) = []
+colsums (x:xs) = work (x:xs) : (colsums (map tail (x:xs)))
+
+work :: [[Integer]] -> [Integer]
+work [] = []
+work (x:xs) = let (f:fs) = x in f : work xs
+
+low2 :: [[Integer]] -> Bool
+low2 [] = True
+low2 (x:xs)
+   | (check (tail aa)) == True = low2 (remCl xs)
    | (check aa) == False = False
  where {
      aa = let (f:fs) = (x:xs) in f;
