d631ea5a3d10f5ced61955ba00fc2f0f8dcc35a4
galvan29 <matteogalvan1999@gmail.com>
2022-04-11 16:03:50 +0200

    push

--- matrici/2.hs
+++ matrici/2.hs
@@ -7,8 +7,3 @@ colsums :: [[Integer]] -> [Integer]
 colsums (x:xs) = let (f:fs) = (changeCol (x:xs)) in (sum f:fs)
 
 changeCol ::  [[Integer]] -> [[Integer]]
-
-<<<<<<< HEAD
-=======
-{- ciao -}
->>>>>>> c5e35fc8df3cf7b48e6b7ca10d7e58cc1933db6f
