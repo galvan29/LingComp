722d8c094265f2b3c80c072a38be41493f1ab71e
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 15:46:59 +0200

    update 7.hs

--- matrici/7.hs
+++ matrici/7.hs
@@ -1,5 +1,5 @@
 {- Si scriva un predicato diagonal che dica se una matrice è diagonale -}
-{- Una matrice diagonale è una matrice in cui sono presenti numeri solamente nella parte diagonale da sinistra-alto fino a sinistra basso -}
+{- Una matrice diagonale è una matrice in cui sono presenti numeri solamente nella parte diagonale da sinistra-alto fino a destra-basso -}
 
 main = do
     print (diagonal [[1,2,3],[4,5,6],[7,8,9]])
