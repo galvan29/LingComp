231781e299c9caac3af00564ec07451510c34a77
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 21:31:49 +0200

    update 8.hs

--- matrici/8.hs
+++ matrici/8.hs
@@ -8,4 +8,4 @@ convergent (x:xs) r = checkSum (x:xs) r
  
 checkSum :: [[Integer]] -> Integer -> Bool
 checkSum [] r = True
-checkSum (x:xs) r = if (foldl (\z y -> if (sqrt (Double)(sum  [ g * g | g <- y])) < r && z==0 then 0 else 1) 0 (x:xs)) == 0 then True else False
+checkSum (x:xs) r = if (foldl (\z y -> if (Floating)(sqrt (sum  [ g * g | g <- y])) < r && z==0 then 0 else 1) 0 (x:xs)) == 0 then True else False
