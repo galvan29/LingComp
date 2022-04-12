7d6f6e13275058324727b9820f09163fa8ebec85
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 21:53:59 +0200

    update 9.hs

---
+++ matrici/9.hs
@@ -0,0 +1,14 @@
+{- let y:ys = x in ciao y:ys -} 
+main = do 
+    print (trasposta [[1,2,3],[4,5,6],[7,8,9]])
+    
+trasposta :: [[Integer]] -> [[Integer]]
+trasposta (x:xs) = colsums (x:xs)
+
+colsums :: [[Integer]] -> [[Integer]]
+colsums ([]:_) = []
+colsums (x:xs) = work (x:xs) : (colsums (map tail (x:xs)))
+
+work :: [[Integer]] -> [Integer]
+work [] = []
+work (x:xs) = let (f:fs) = x in f : work xs
