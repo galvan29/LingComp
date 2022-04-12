815e5c3cb253d496adbf9a864445867c6206ddb5
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 21:50:01 +0200

    update 8.hs

--- matrici/8.hs
+++ matrici/8.hs
@@ -1,6 +1,6 @@
 main = do
     print (convergent [[1,2,3],[4,5,6],[7,8,9]] 1000)
-    print (convergent [[1,2,3],[4,5,6],[7,8,9]] 15)
+    print (convergent [[1,2,3],[4,5,6],[7,8,9]] 10)
 {-    print ((sum (h * h | h <- [1,2,3])))   -}
     
 convergent :: [[Double]] -> Double -> Bool
