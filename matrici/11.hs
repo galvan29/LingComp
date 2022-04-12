dd79b9edd014efd58192b7356cb21dbfffdf0893
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 23:03:29 +0200

    update 11.hs

--- matrici/11.hs
+++ matrici/11.hs
@@ -2,3 +2,26 @@ main = do
     print (moltipl [[1,0,1],[2,3,4]] [[1,2],[4,1],[0,1]])
     
 moltipl :: [[Integer]] -> [[Integer]] -> [[Integer]]
+moltipl (x:xs) (y:ys) = b (x:xs) (colsums (y:ys))
+
+b :: [[Integer]] -> [[Integer]] -> [[Integer]]
+b [] [] = []
+b (x:xs) (y:ys) = c x (y:ys) ++ b xs (y:ys)
+
+c :: [Integer] -> [[Integer]] -> [Integer]
+c (x:xs) [] = []
+c (x:xs) (y:ys) = (somma x y) : c (x:xs) ys
+
+somma :: [Integer] -> [Integer] -> Integer
+somma (x:[]) (y:[]) = x + y
+somma (x:xs) y = x*y + somma xs ys
+
+
+
+colsums :: [[Integer]] -> [[Integer]]
+colsums ([]:_) = []
+colsums (x:xs) = work (x:xs) : (colsums (map tail (x:xs)))
+
+work :: [[Integer]] -> [Integer]
+work [] = []
+work (x:xs) = let (f:fs) = x in f : work xs
