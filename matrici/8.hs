61562a728c730e7436c2e8bbd42c5a3a58dafa17
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 17:02:07 +0200

    update 8.hs

--- matrici/8.hs
+++ matrici/8.hs
@@ -1,10 +1,11 @@
 main = do
     print (convergent [[1,2,3],[4,5,6],[7,8,9]] 30)
     print (convergent [[1,2,3],[4,5,6],[7,8,9]] 15)
+    print ( (abs (sum ( g * g | g <- [1,2,3]))))
     
 convergent :: [[Integer]] -> Integer -> Bool
 convergent (x:xs) r = checkSum (x:xs) r
  
 checkSum :: [[Integer]] -> Integer -> Bool
 checkSum [] r = True
-checkSum (x:xs) r = if (foldl (\z y -> if (abs (sum ( g * g | g <- x))) < r then 0 else 1) 0 (xs)) == 0 then (checkSum (xs) r) else False
+checkSum (x:xs) r = if (foldl (\z y -> if (abs (sum x)) < r then 0 else 1) 0 (xs)) == 0 then (checkSum (xs) r) else False
