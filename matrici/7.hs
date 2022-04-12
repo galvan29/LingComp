87e194163524491883238175eb73f7c015d5dc1f
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 15:49:38 +0200

    update 7.hs

--- matrici/7.hs
+++ matrici/7.hs
@@ -2,4 +2,16 @@
 {- Una matrice diagonale è una matrice in cui sono presenti numeri solamente nella parte diagonale da sinistra-alto fino a destra-basso -}
 
 main = do
-    print (diagonal [[1,2,3],[4,5,6],[7,8,9]])
+    print (diagonal [[1,0,0],[0,5,0],[0,0,9]])
+    print (diagonal [[1,0,0],[0,5,0],[0,1,9]])
+
+diagonal :: [[Integer]] -> Bool
+diagonal (x:xs) = if ((a==True) && (b==True)) then True else False
+ where {
+    a = lovertriangular (x:xs)
+    b = 
+ }
+ 
+
+lovertriangular [] = True
+lovertriangular (x:xs) = if (foldl (\z y->if z==y then 0 else 1) 0 (tail x))==0 then lovertriangular (map tail xs) else False
