25d3bd96619c0fa29f6a80bee8bb746dce5e4e2a
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 21:50:54 +0200

    update 8.hs

--- matrici/8.hs
+++ matrici/8.hs
@@ -1,6 +1,6 @@
 main = do
     print (convergent [[1,2,3],[4,5,6],[7,8,9]] 1000)
-    print (convergent [[1,2,3],[4,5,6],[7,8,9]] 8)
+    print (convergent [[1,2,3],[4,5,6],[7,8,9]] 15)
 {-    print ((sum (h * h | h <- [1,2,3])))   -}
     
 convergent :: [[Double]] -> Double -> Bool
@@ -8,7 +8,7 @@ convergent (x:xs) r = checkSum (x:xs) r (mainDiagonal (x:xs))
  
 checkSum :: [[Double]] -> Double -> [Double] -> Bool
 checkSum [] r [] = True
-checkSum (x:xs) r (h:hs) = if (sqrt ((sum  [ g * g | g <- x])) - (h*h)) < r then checkSum xs r hs else False
+checkSum (x:xs) r (h:hs) = if (sqrt ((sum  [ g * g | g <- x]) - (h*h))) < r then checkSum xs r hs else False
 
 
 mainDiagonal xs = zipWith (!!) xs [0..]
