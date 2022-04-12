717311ce08c435b73c5057d377b8c85484c01a28
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 22:04:42 +0200

    update 10.hs

---
+++ matrici/10.hs
@@ -0,0 +1,18 @@
+main = do
+   print (isSymmetric [[1,2,3],[1,2,3]])
+
+isSymmetric :: [[Integer]] -> Bool
+isSymmetric (x:xs) = ciao (x:xs) (colsums (x:xs))
+
+ciao :: [[Integer]] -> [[Integer]] -> Bool
+ciao [] [] = True
+ciao (x:xs) (y:ys) = if ( x == y ) == True then ciao (xs) (ys) else False
+
+
+colsums :: [[Integer]] -> [[Integer]]
+colsums ([]:_) = []
+colsums (x:xs) = work (x:xs) : (colsums (map tail (x:xs)))
+
+work :: [[Integer]] -> [Integer]
+work [] = []
+work (x:xs) = let (f:fs) = x in f : work xs
