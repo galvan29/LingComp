bc37f5855b36da3647c543faf03fbf16e7b2caa8
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-12 17:05:27 +0200

    update 8.hs

--- matrici/8.hs
+++ matrici/8.hs
@@ -1,7 +1,8 @@
 main = do
     print (convergent [[1,2,3],[4,5,6],[7,8,9]] 30)
     print (convergent [[1,2,3],[4,5,6],[7,8,9]] 15)
-    print ((sum (h * h | h <- [1,2,3])))
+{-    print ((sum (h * h | h <- [1,2,3])))   -}
+    print [ x * x | x <- [2,4..18]]
     
 convergent :: [[Integer]] -> Integer -> Bool
 convergent (x:xs) r = checkSum (x:xs) r
