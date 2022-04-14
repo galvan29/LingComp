3a6f1dde15b51d44860ce71813c1aeb67d9b625a
galvan29 <matteogalvan1999@gmail.com>
2022-04-14 22:38:43 +0200

    ss

--- bst/10.hs
+++ bst/10.hs
@@ -1,3 +1,25 @@
 {- 10. Si scriva un predicato (funzione a valori booleani) almostBalanced per determinare se un albero
 binario ha la seguente proprietà: per ogni nodo le altezze dei figli destro e sinistro differiscono al
 massimo di 1. -}
+
+data BST a = Void | Node { val :: a, left, right :: BST a } deriving (Eq, Ord, Read, Show)
+
+main = do
+   print (almostBalanced (Node 10 (Node 5 Void Void) (Node 15 Void Void)))
+
+almostBalanced :: (Num a, Ord a) => BST a -> Bool
+almostBalanced Void = True
+almostBalanced (Node x l r) = if ((c-d) == -1 || (c-d) == 1 || (c-d) == 0) then (a && b) else False
+  where {
+      a = almostBalanced l;
+      b = almostBalanced r;
+      c = conta 0 l;
+      d = conta 0 r;
+  }
+
+conta :: (Ord a, Num a) => a -> BST a -> a
+conta v Void = v
+conta v (Node x Void Void) = v+1
+conta v (Node x Void r) = conta (v+1) r
+conta v (Node x l Void) = conta (v+1) l
+conta v (Node x l r) = if(conta (v+1) l > conta (v+1) r) then conta (v+1) l else conta (v+1) r
--- bst/9.hs
+++ bst/9.hs
@@ -1,7 +1,8 @@
 data BST a = Void | Node { val :: a, left, right :: BST a } deriving (Eq, Ord, Read, Show)
 
 main = do
-   print (annotate (Node 10 (Node 5 (Node 2 Void (Node 3 Void Void)) Void) (Node 15 (Node 12 Void Void) Void)))
+   print (annotate (Node 10 (Node 5 (Node 2 Void Void) Void) Void))
+   print (conta 0 (Node 10 (Node 5 (Node 2 Void Void) Void) Void))
 
 annotate :: (Num a, Ord a) => BST a -> BST (a,a)
 annotate Void = Void
@@ -9,11 +10,13 @@ annotate (Node x l r) = Node (x, if (c > d) then c else d) a b
   where {
       a = annotate l;
       b = annotate r;
-      c = conta 1 l;
-      d = conta 1 r;
+      c = conta 0 l;
+      d = conta 0 r;
   }
 
 conta :: (Ord a, Num a) => a -> BST a -> a
 conta v Void = v
+conta v (Node x Void Void) = v+1
 conta v (Node x Void r) = conta (v+1) r
 conta v (Node x l Void) = conta (v+1) l
+conta v (Node x l r) = if(conta (v+1) l > conta (v+1) r) then conta (v+1) l else conta (v+1) r
