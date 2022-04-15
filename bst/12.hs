f739cb326de79fe6e4cc1ccc921a29b189cb1538
galvan29 <matteogalvan1999@gmail.com>
2022-04-15 11:36:43 +0200

    casca

--- bst/12.hs
+++ bst/12.hs
@@ -8,3 +8,30 @@
       restituisce la soluzione
    Node (4,Just 1) Void (Node (7,Nothing) (Node (5,Just 2) Void Void) Void). 
 -}
+
+data BST a = Void | Node { val :: a, left, right :: BST a } deriving (Eq, Ord, Read, Show)
+
+main = do
+   print (diff2next (Node 4 (Node 3 Void Void) (Node 5 Void Void)))
+   print (diff2next (Node 10 (Node 5 Void Void) (Node 15 Void Void)))
+
+
+diff2next :: (Integral a, Num a, Show a) => BST a -> BST (a,String)
+diff2next Void = Void
+diff2next (Node x l r) = work 0 (Node x l r)
+
+work :: (Integral a, Num a, Show a) => a -> BST a -> BST (a,String)
+work b Void = Void 
+work b (Node x l r) = Node (x,if(ciao)/=0 then ("Just "++(show (ciao-x))) else check) (work x l) (work 0 r)
+ where {
+    ciao = scendi (diff r) r;
+    check = if(b-x)>0 then "Just "++(show (b-x)) else ("Nothing")
+ }
+
+scendi :: (Integral a, Num a) => a -> BST a -> a
+scendi v Void = v
+scendi v (Node x l r) = scendi x l
+
+diff :: (Integral a, Ord a, Num a) => BST a -> a
+diff Void = 0
+diff (Node x l r) = x
