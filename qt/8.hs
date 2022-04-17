d89450ade369e7db3d91a055e17e6df1af05d71e
galvan29 <matteogalvan1999@gmail.com>
2022-04-17 13:10:22 +0200

    sss

--- qt/8.hs
+++ qt/8.hs
@@ -1,10 +1,10 @@
-{- 7. Si scriva una funzione Haskell difference che dato un colore c ed un QuadTree q determina la
-   differenza fra il numero di pixel dell’immagine codificata da q che hanno un colore maggiore di c e
-   quelli minori di c. Ad esempio
-   let d = C 2; u = C 1; q = Q d u u u
-   in difference 1 ( Q q ( C 0) (C 3) q )
-   restituisce -4 (visto che il QuadTree codifica almeno 16 pixel). 
--}
+{- 8. Si scriva una funzione Haskell overColor che dato un colore c ed un QuadTree q determina il
+numero (minimo) di pixel dell’immagine codificata da q che hanno un colore maggiore di c. Ad
+esempio
+let d = C 2; u = C 1; q = Q d u u u
+in overColor 1 ( Q q ( C 0) ( C 3) q )
+restituisce 6 (visto che il QuadTree codifica almeno 16 pixel). -}
+
 
 data QT a = C a | Q (QT a) (QT a) (QT a) (QT a) deriving (Eq, Show)
 
@@ -14,14 +14,7 @@ main = do
    print (difference 1 f)
 
 difference :: (Eq a, Num a, Ord a, Integral a) => a -> QT a -> a
-difference val (Q a b c d) = (occurrencies (Q a b c d) val) - (occurrencies3 (Q a b c d) val)
-
-occurrencies3 :: (Eq a, Num a, Ord a, Integral a) => QT a -> a -> a
-occurrencies3 (Q a b c d) v = occurrencies4 (Q a b c d) v ((howManyPixels (Q a b c d)))
-
-occurrencies4 :: (Eq a, Num a, Ord a, Integral a) => QT a -> a -> a -> a
-occurrencies4 (C k) v liv = if(k <= v) then 4^(liv) else 0
-occurrencies4 (Q a b c d) v liv = (occurrencies4 a v (liv-1)) + (occurrencies4 b v (liv-1)) + (occurrencies4 c v (liv-1)) + (occurrencies4 d v (liv-1))
+difference val (Q a b c d) = (occurrencies (Q a b c d) val)
 
 occurrencies :: (Eq a, Num a, Ord a, Integral a) => QT a -> a -> a
 occurrencies (Q a b c d) v = occurrencies2 (Q a b c d) v ((howManyPixels (Q a b c d)))
