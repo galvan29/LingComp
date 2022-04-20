a6aedd0c63658d4fbf219e6e552a70bc0f228cf1
galvan29 <matteogalvan1999@gmail.com>
2022-04-20 15:52:17 +0200

    aaa

--- bst/14.hs
+++ bst/14.hs
@@ -1,8 +1,16 @@
 {- 14. Si scriva una funzione treeheight per calcolare l’altezza di un albero usando opportunamente fold. -}
 
-main = do
-   print (ciao [[1,0,0]])
+data BST a = Void | Node {val :: a, left, right :: BST a} deriving (Eq, Ord, Read, Show)
 
-ciao :: [[Integer]] -> Bool
-ciao (x:xs) = if (foldl (\z y->if z==y then 0 else 1) 0 (tail x))==1 then True else False
+fold :: (Ord a) = > (a -> b -> b -> b) -> b -> BST a -> b
+fold _ z Void = z
+fold f z (Node x l r) = f x (fold f z l) (fold f z r)
+
+
+
+main = do
+   print (ciao [[1,1,1]])
+
+treeheight :: (Ord a, Num a, Integer a) => BST a -> a
+treeheight (Node x l r) = if (foldl (\z y->if z==y then 0 else 1) 0 (tail x))==0 then True else False
 
