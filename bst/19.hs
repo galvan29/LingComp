b0f7a7373a856d26798089de2b96e33a7b995ed3
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-21 09:14:28 +0200

    update 19.hs

--- bst/19.hs
+++ bst/19.hs
@@ -1,7 +1,9 @@
-data BST a = Void | Node {val :: a, left, right :: BST a} deriving (Eq, Ord, Read, Show)
+{- 19. Si scriva un predicato isAVL che dato un albero secondo la seguente definizione di tipo 
+data (Ord a) => ABST a = Void | Node Bal a (ABST a) (ABST a) deriving (Eq, Ord, Read, Show) data Bal = Left | Bal | Right deriving (Eq, Ord, Read, Show) determina se è ben formato, cioè se • la differenza fra le profondità dei sottoalberi destro e sinistro di un qualunque nodo è al massimo 1; • le etichette Bal dei nodi sono consistenti con lo (s)bilanciamento. -}
 
-fold :: (Ord a) => (a -> b -> b -> b) -> b -> BST a -> Bool
-fold _ z Void = z
-fold f z (Node x l r) = f x (fold f z l) (fold f z r)
+
+
+data ABST a = Void | Node Bal a (ABST a) (ABST a) deriving (Eq, Ord, Read, Show) 
+data Bal = Left | Bal | Right deriving (Eq, Ord, Read, Show)
 
 isBST :: (Ord a, Num a, Integral a, Eq a, Show a) => 
