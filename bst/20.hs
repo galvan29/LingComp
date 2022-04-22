66340e4fb53e66bb27c35bc875f5c22579caeb04
galvan29 <matteogalvan1999@gmail.com>
2022-04-22 15:45:20 +0200

    sdvsdv

--- bst/20.hs
+++ bst/20.hs
@@ -9,19 +9,25 @@ data RBT a = Void | Node a Color (RBT a) (RBT a) deriving (Eq, Ord, Read, Show)
 data Color = Red | Black deriving (Eq, Ord, Read, Show)
 
 main = do
-   print (RBT (Node 10 Black (Node 5 Black (Node 5 Black Void Void) (Node 5 Black Void Void)) (Node 15 Black (Node 5 Black Void Void) (Node 5 Black Void Void))))
+   print (isRBT (Node 10 Black (Node 5 Black Void Void) (Node 15 Black Void Void)))
+   print (isRBT (Node 10 Black (Node 6 Black (Node 3 Red Void Void) (Node 8 Red (Node 7 Black Void Void) (Node 9 Black Void Void))) (Node 15 Black (Node 12 Black Void Void) (Node 17 Black Void Void))))
+   print (isRBT (Node 6 Black (Node 3 Black Void Void) (Node 8 Red (Node 7 Black Void Void) (Node 9 Black Void Void))))
 
-isRBT :: (Eq a, Ord a, Num a) => RBT a -> a
+isRBT :: (Eq a, Ord a, Num a) => RBT a -> Bool
 isRBT (Node x color l r) = rbheight (Node x color l r)
 
 
-rbheight :: (Eq a, Ord a, Num a) => RBT a -> a
-rbheight Void = 1
-rbheight (Node x color l r) = 0 + if((rbheight l)==(rbheight r)) then (if(color==Black) then 1 else 0) else 0
+rbheight :: (Eq a, Ord a, Num a) => RBT a -> Bool
+rbheight Void = True
+rbheight (Node x color l r) = if((conta Black 0 l)==(conta Black 0 r)) then ((rbheight l) && (rbheight r)) else False
 
-conta :: (Ord a, Num a) => a -> RBT a -> a
-conta v Void = v
-conta v (Node x color Void Void) = if(color==Black) then v+1 else v
-conta v (Node x Void r) = if(color==Black) then (conta (v+1) r) else (conta v r)
-conta v (Node x l Void) = if(color==Black) then (conta (v+1) l) else (conta v l)
-conta v (Node x l r) = if(conta (v+1) l == conta (v+1) r) then v
+conta :: (Ord a, Num a) => Color -> a -> RBT a -> a
+conta col v Void = (v+1)
+conta col v (Node x color Void Void) = if(color==Black) then (v+1) else (v)
+conta col v (Node x color Void r) = if(color==Black) then (conta col (v+1) r)+0 else 0+(conta col v r)
+conta col v (Node x color l Void) = if(color==Black) then (conta col (v+1) l)+0 else 0+(conta col v l)
+conta col v (Node x color l r) = if(color==Black) then ((conta col (v+1) l)+tr-tr) else ((conta col v l)+hh-hh)
+  where {
+      tr = (conta col (v+1) r);
+      hh = (conta col v r);
+  }
