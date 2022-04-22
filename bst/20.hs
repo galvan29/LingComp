f794bc017fa2fe551d0b208853d38dc724da5633
galvan29 <matteogalvan1999@gmail.com>
2022-04-22 21:03:50 +0200

    hh

--- bst/20.hs
+++ bst/20.hs
@@ -12,6 +12,7 @@ main = do
    print (isRBT (Node 10 Black (Node 5 Black Void Void) (Node 15 Black Void Void)))
    print (isRBT (Node 10 Black (Node 6 Black (Node 3 Red Void Void) (Node 8 Red (Node 7 Black Void Void) (Node 9 Black Void Void))) (Node 15 Black (Node 12 Black Void Void) (Node 17 Black Void Void))))
    print (isRBT (Node 6 Black (Node 3 Black Void Void) (Node 8 Red (Node 7 Black Void Void) (Node 9 Black Void Void))))
+   print (checkRed Black (Node 10 Black (Node 5 Black Void Void) (Node 15 Black (Node 13 Red (Node 11 Black Void Void) (Node 14 Black Void Void)) (Node 17 Black Void Void))))
 
 isRBT :: (Eq a, Ord a, Num a) => RBT a -> Bool
 isRBT (Node x color l r) = rbheight (Node x color l r)
@@ -19,15 +20,19 @@ isRBT (Node x color l r) = rbheight (Node x color l r)
 
 rbheight :: (Eq a, Ord a, Num a) => RBT a -> Bool
 rbheight Void = True
-rbheight (Node x color l r) = if((conta Black 0 l)==(conta Black 0 r)) then ((rbheight l) && (rbheight r)) else False
+rbheight (Node x color l r) = if((conta 0 l)==(conta 0 r)) then ((rbheight l) && (rbheight r)) else False
 
-conta :: (Ord a, Num a) => Color -> a -> RBT a -> a
-conta col v Void = (v+1)
-conta col v (Node x color Void Void) = if(color==Black) then (v+1) else (v)
-conta col v (Node x color Void r) = if(color==Black) then (conta col (v+1) r)+0 else 0+(conta col v r)
-conta col v (Node x color l Void) = if(color==Black) then (conta col (v+1) l)+0 else 0+(conta col v l)
-conta col v (Node x color l r) = if(color==Black) then ((conta col (v+1) l)+tr-tr) else ((conta col v l)+hh-hh)
-  where {
-      tr = (conta col (v+1) r);
-      hh = (conta col v r);
-  }
+conta :: (Ord a, Num a) => a -> RBT a -> a
+conta v Void = (v+1)
+conta v (Node x color Void Void) = if(color==Black) then (v+1) else (v)
+conta v (Node x color Void r) = if(color==Black) then (conta (v+1) r) else (conta v r)
+conta v (Node x color l Void) = if(color==Black) then (conta (v+1) l) else (conta v l)
+conta v (Node x color l r) = if(color==Black) then (conta (v+1) l) else ((conta v l))
+
+checkRed :: (Eq a, Ord a, Num a) => Color -> RBT a -> Bool
+checkRed col Void = True
+checkRed col (Node x color Void Void) = if(color==Black) then True else if(color==Red && col==Black) then True else False
+checkRed col (Node x color l r) = if(color==Black) then ye else if (color==Red && col==Black) then ye else False 
+ where {
+     ye = ((checkRed color l) && (checkRed color r));
+ }
