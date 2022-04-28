c38566d435c5622b71599fb0036f66ceebbec529
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-28 11:56:21 +0200

    update 7.hs

--- qtM/7.hs
+++ qtM/7.hs
@@ -9,6 +9,7 @@ main = do
    print (f [1,2,3,5] (Mat 1 (Q (C 1) (C 1) (C 1) (C 1))))
    print (convert 2 ww)
    print (getRow 2 3 (convert 2 ww))
+   print (createList w2)
 
 f :: (Num a, Eq a, Show a, Ord a) => [a] -> Mat a -> a
 f array (Mat n (Q a b c d)) = 4
