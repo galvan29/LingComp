6a23c388f377a1071ad970b74c25bcb8634a647c
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-29 11:43:52 +0200

    update 10.hs

---
+++ qtM/10.hs
@@ -0,0 +1,21 @@
+data QT a = C a | Q (QT a) (QT a) (QT a) (QT a) deriving (Eq, Show)  
+data Mat a = Mat {nexp :: Int, mat :: QT a} deriving ( Eq , Show )
+
+main = do
+   let w2 = (Q (C 2) (C 2) (C 2) (C 2))
+   let w0 = (Q (C 0) (C 0) (C 0) (C 0))
+   let w1 = (Q (C 1) (C 1) (C 1) (C 1))
+   let ww = (Q w1 (C 0) w0 w2)
+   print (rowSum (Mat 2 ww))
+
+rowSum :: (Eq a, Show a, Num a) => Mat a -> [a]
+rowSum m = csum (mat m)
+    where
+        n = nexp m
+        csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
+        csum (Q a b c d) = zipWith (+) ((rowSum $ submat a) ++ (rowSum $ submat c))
+                                       ((rowSum $ submat b) ++ (rowSum $ submat d))
+        submat q = Mat (n - 1) q
+
+searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
+searchMinMax 
