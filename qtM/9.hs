818544de143f20948c5d58e5d881a37dd6ccecd5
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-29 11:38:17 +0200

    update 9.hs

--- qtM/9.hs
+++ qtM/9.hs
@@ -6,13 +6,13 @@ main = do
    let w0 = (Q (C 0) (C 0) (C 0) (C 0))
    let w1 = (Q (C 1) (C 1) (C 1) (C 1))
    let ww = (Q w1 (C 0) w0 w2)
-   print (colsum (Mat 2 ww))
+   print (rowSum (Mat 2 ww))
 
-colsum :: (Eq a, Show a, Num a) => Mat a -> [a]
-colsum m = csum (mat m)
+rowSum :: (Eq a, Show a, Num a) => Mat a -> [a]
+rowSum m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (+) ((colsum $ submat a) ++ (colsum $ submat c))
-                                       ((colsum $ submat b) ++ (colsum $ submat d))
+        csum (Q a b c d) = zipWith (+) ((rowSum $ submat a) ++ (rowSum $ submat c))
+                                       ((rowSum $ submat b) ++ (rowSum $ submat d))
         submat q = Mat (n - 1) q
