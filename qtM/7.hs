3b7472c36b6b940885964e3c5a729c336ecf4abe
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-29 11:06:40 +0200

    update 7.hs

--- qtM/7.hs
+++ qtM/7.hs
@@ -17,8 +17,8 @@ colsum m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (+) ((colsum $ submat a) ++ (colsum $ submat b))
-                                       ((colsum $ submat c) ++ (colsum $ submat d))
+        csum (Q a b c d) = zipWith (+) ((colsum $ submat a) ++ (colsum $ submat c))
+                                       ((colsum $ submat b) ++ (colsum $ submat d))
         submat q = Mat (n - 1) q
 
 
