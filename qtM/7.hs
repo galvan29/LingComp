3b515f6b09c5dd94502bcdba84759707c38f339c
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-29 10:12:52 +0200

    update 7.hs

--- qtM/7.hs
+++ qtM/7.hs
@@ -18,13 +18,13 @@ convert :: (Num a, Eq a, Show a, Ord a) => Int -> QT a -> QT a
 convert n (C x) = if(n==1) then (Q (C x) (C x) (C x) (C x)) else (Q (convert (n-1) (C x)) (convert (n-1) (C x)) (convert (n-1) (C x)) (convert (n-1) (C x)))
 convert n (Q x1 x2 x3 x4) = if(n==1) then (Q x1 x2 x3 x4) else (Q (convert (n-1) x1) (convert (n-1) x2) (convert (n-1) x3) (convert (n-1) x4))
 
-
 createList :: (Num a, Eq a, Show a, Ord a) => QT a -> QT a -> QT a -> QT a -> [[QT a]]
 createList a b c d = [[a,b],[c,d]]
 
+
 createL :: (Num a, Eq a, Show a, Ord a) => QT a -> [a]
 createL (C a) = [a]
-createL (Q a b c d) = createL a ++ createL b ++ createL c ++ createL d
+createL (Q a b c d) = [createL a ++ createL b ++ createL c ++ createL d]
 
 
 
