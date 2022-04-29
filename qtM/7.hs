b0e42d652ac6b67b7570c7d72beae3839981c24a
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-29 10:09:28 +0200

    update 7.hs

--- qtM/7.hs
+++ qtM/7.hs
@@ -23,7 +23,7 @@ createList :: (Num a, Eq a, Show a, Ord a) => QT a -> QT a -> QT a -> QT a -> [[
 createList a b c d = [[a,b],[c,d]]
 
 createL :: (Num a, Eq a, Show a, Ord a) => QT a -> [[a]]
-createL (Q (C a) (C b) (C c) (C d)) = [[a,b,c,d]]
+createL (Q (C a) (C b) (C c) (C d)) = [a,b,c,d]
 createL (Q a b c d) = createL a ++ createL b ++ createL c ++ createL d
 
 
