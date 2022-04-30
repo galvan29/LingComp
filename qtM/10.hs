1f662212a5a23e08ec7cdab6af0c10d51ca6f31a
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 18:53:06 +0200

    update 10.hs

--- qtM/10.hs
+++ qtM/10.hs
@@ -8,13 +8,14 @@ main = do
    let ww = (Q w1 (C 0) w0 w2)
    print (rowSum (Mat 2 ww))
 
-rowSum :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+rowSum :: (Eq a, Show a, Num a) => Mat a -> [a]
 rowSum m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) [1] [2]
+        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((rowSum $ submat a) ++ (rowSum $ submat c))
+                                       ((rowSum $ submat b) ++ (rowSum $ submat d))
         submat q = Mat (n - 1) q
 
-{-  searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
+{- searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
 searchMinMax -}
