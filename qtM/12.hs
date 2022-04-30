99c73cc3fb59095693e77e39c758fe4a7af6bd56
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 19:17:37 +0200

    update 12.hs

--- qtM/12.hs
+++ qtM/12.hs
@@ -7,14 +7,14 @@ main = do
    let w1 = (Q (C 1) (C 1) (C 1) (C 1))
    let ww = (Q w1 (C 0) w0 w2)
    let mat3 = Mat 2 ww
-   print (searchMinMax mat3)
+   print (altSumMax mat3)
 
 
-searchMin :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
-searchMin m = csum (mat m)
+altSum :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+altSum m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (\x y -> x-y) ((searchMin $ submat a) ++ (searchMin $ submat c))
-                                       ((searchMin $ submat b) ++ (searchMin $ submat d))
+        csum (Q a b c d) = zipWith (\x y -> x-y) ((altSum $ submat a) ++ (altSum $ submat c))
+                                       ((altSum $ submat b) ++ (altSum $ submat d))
         submat q = Mat (n - 1) q
