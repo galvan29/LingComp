6f65dc1639e1e0c81b6f23845286fd1895784c8d
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 19:17:56 +0200

    update 12.hs

--- qtM/12.hs
+++ qtM/12.hs
@@ -7,7 +7,7 @@ main = do
    let w1 = (Q (C 1) (C 1) (C 1) (C 1))
    let ww = (Q w1 (C 0) w0 w2)
    let mat3 = Mat 2 ww
-   print (altSumMax mat3)
+   print (altSum mat3)
 
 
 altSum :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
@@ -15,6 +15,6 @@ altSum m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (\x y -> x-y) ((altSum $ submat a) ++ (altSum $ submat c))
-                                       ((altSum $ submat b) ++ (altSum $ submat d))
+        csum (Q a b c d) = zipWith (\x y -> x-y) ((altSum $ submat a) ++ (altSum $ submat b))
+                                       ((altSum $ submat c) ++ (altSum $ submat d))
         submat q = Mat (n - 1) q
