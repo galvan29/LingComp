79e0322e3765a1d2300aa4236ecb6ce517bc6032
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 19:04:33 +0200

    update 11.hs

---
+++ qtM/11.hs
@@ -0,0 +1,35 @@
+data QT a = C a | Q (QT a) (QT a) (QT a) (QT a) deriving (Eq, Show)  
+data Mat a = Mat {nexp :: Int, mat :: QT a} deriving ( Eq , Show )
+
+main = do
+   let w2 = (Q (C 2) (C 2) (C 2) (C 2))
+   let w0 = (Q (C 0) (C 0) (C 0) (C 0))
+   let w1 = (Q (C 1) (C 1) (C 1) (C 1))
+   let ww = (Q w1 (C 0) w0 w2)
+   let mat3 = Mat 2 ww
+   print (searchMinMax mat3)
+
+searchMax :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+searchMax m = csum (mat m)
+    where
+        n = nexp m
+        csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
+        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((searchMax $ submat a) ++ (searchMax $ submat c))
+                                       ((searchMax $ submat b) ++ (searchMax $ submat d))
+        submat q = Mat (n - 1) q
+
+searchMin :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+searchMin m = csum (mat m)
+    where
+        n = nexp m
+        csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
+        csum (Q a b c d) = zipWith (\x y -> if(x<y) then x else y) ((searchMin $ submat a) ++ (searchMin $ submat c))
+                                       ((searchMin $ submat b) ++ (searchMin $ submat d))
+        submat q = Mat (n - 1) q
+
+searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
+searchMinMax mat = createCouple (searchMin mat) (searchMax mat)
+
+createCouple :: (Eq a, Show a, Num a, Ord a) => [a] -> [a] ->[(a,a)]
+createCouple (x:[]) (y:[]) = [(x-y)]
+createCouple (x:xs) (y:ys) = (x-y) : createCouple xs ys
