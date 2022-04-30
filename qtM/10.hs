5c7289880be20aa86a2cb7777bd1a60bc3713154
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 18:49:31 +0200

    update 10.hs

--- qtM/10.hs
+++ qtM/10.hs
@@ -13,9 +13,9 @@ rowSum m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (+) ((rowSum $ submat a) ++ (rowSum $ submat c))
+        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((rowSum $ submat a) ++ (rowSum $ submat c))
                                        ((rowSum $ submat b) ++ (rowSum $ submat d))
         submat q = Mat (n - 1) q
 
 searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
-searchMinMax 
+{- searchMinMax -}
