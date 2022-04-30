c5eedf6f5ed3f381a014c9ec6f1948690bad2aa8
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 18:57:02 +0200

    update 10.hs

--- qtM/10.hs
+++ qtM/10.hs
@@ -6,15 +6,24 @@ main = do
    let w0 = (Q (C 0) (C 0) (C 0) (C 0))
    let w1 = (Q (C 1) (C 1) (C 1) (C 1))
    let ww = (Q w1 (C 0) w0 w2)
-   print (rowSum (Mat 2 ww))
+   print (searchMax (Mat 2 ww))
 
-rowSum :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
-rowSum m = csum (mat m)
+searchMax :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+searchMax m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((rowSum $ submat a) ++ (rowSum $ submat c))
-                                       ((rowSum $ submat b) ++ (rowSum $ submat d))
+        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((searchMax $ submat a) ++ (searchMax $ submat c))
+                                       ((searchMax $ submat b) ++ (searchMax $ submat d))
+        submat q = Mat (n - 1) q
+
+searchMin :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+searchMin m = csum (mat m)
+    where
+        n = nexp m
+        csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
+        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((searchMin $ submat a) ++ (searchMin $ submat c))
+                                       ((searchMin $ submat b) ++ (searchMin $ submat d))
         submat q = Mat (n - 1) q
 
 {- searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
