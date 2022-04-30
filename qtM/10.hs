6c2aaf3ee8d76dd5eae92434e871e9eee700a4e3
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-04-30 19:16:22 +0200

    update 10.hs

--- qtM/10.hs
+++ qtM/10.hs
@@ -14,8 +14,8 @@ searchMax m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((searchMax $ submat a) ++ (searchMax $ submat c))
-                                       ((searchMax $ submat b) ++ (searchMax $ submat d))
+        csum (Q a b c d) = zipWith (\x y -> if(x>y) then x else y) ((searchMax $ submat a) ++ (searchMax $ submat b))
+                                       ((searchMax $ submat c) ++ (searchMax $ submat d))
         submat q = Mat (n - 1) q
 
 searchMin :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
@@ -23,8 +23,8 @@ searchMin m = csum (mat m)
     where
         n = nexp m
         csum (C c)       = take (2 ^ n) $ repeat (c * 2 ^ n)
-        csum (Q a b c d) = zipWith (\x y -> if(x<y) then x else y) ((searchMin $ submat a) ++ (searchMin $ submat c))
-                                       ((searchMin $ submat b) ++ (searchMin $ submat d))
+        csum (Q a b c d) = zipWith (\x y -> if(x<y) then x else y) ((searchMin $ submat a) ++ (searchMin $ submat b))
+                                       ((searchMin $ submat c) ++ (searchMin $ submat d))
         submat q = Mat (n - 1) q
 
 searchMinMax :: (Eq a, Num a, Show a, Ord a) => Mat a -> [(a,a)]
