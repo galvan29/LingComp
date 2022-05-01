2355a18e2b498ff4bf12dfe624ed73401bf92b55
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-01 18:48:20 +0200

    update 13.hs

--- qtM/13.hs
+++ qtM/13.hs
@@ -7,9 +7,9 @@ main = do
    let w1 = (Q (C 1) (C 0) (C 0) (C 2))
    let ww = (Q w1 (Q (C 1) (C 0) (C 3) (C 7)) w0 (Q (C 3) (C 1) (C 1) (C 0)))
    let mat3 = Mat 2 ww
-   print (transpose mat3)
+   print (transpose ww)
 
-transpose :: (Eq a, Show a, Num a, Ord a) => Mat a -> Mat a
-transpose (Mat n (C x)) = Mat n (C x)
-transpose (Mat n (Q a b c d)) = Mat n (Q (transpose (Mat (n-1) a)) (transpose (Mat (n-1) b)) (transpose (Mat (n-1) c)) (transpose (Mat (n-1) d)))
+transpose :: (Eq a, Show a, Num a, Ord a) => QT a -> QT a
+transpose (C x) = (C x)
+transpose (Q a b c d) = (Q (transpose a) (transpose b) (transpose c) (transpose d))
         
