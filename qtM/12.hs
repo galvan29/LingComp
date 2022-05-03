7c25248bded41962d801f9dd2939a3d73640a0c0
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-03 16:19:14 +0200

    update 12.hs

--- qtM/12.hs
+++ qtM/12.hs
@@ -9,7 +9,7 @@ main = do
    let mat3 = Mat 2 ww
    print (colAltSum mat3)
 
-colAltSum :: (Eq a, Show a, Num a, Ord a) => Mat a -> [a]
+colAltSum :: (Eq a, Show a, Num a, Ord a, Integral a) => Mat a -> [a]
 colAltSum (Mat n (Q a b c d))= work n (convert n (Q a b c d))
 
 work :: (Eq a, Show a, Num a, Ord a, Integral a) => Int -> QT a -> [a]
