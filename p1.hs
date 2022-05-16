11be2d260c64b5847a023094b1e72931b4e37c49
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-16 17:16:44 +0200

    update p1.hs

--- p1.hs
+++ p1.hs
@@ -5,7 +5,7 @@ main = do
    let z = (Q (C 1) (C 0) (C 0) (C 1))
    print (diagonal (Mat 2 (Q z (C 0) (C 0) z)))
 
-diagonal (Mat n (Q x1 x2 x3 x4)) = if(dNZero (Mat n (Q x1 x2 x3 x4))) && (uLZero (Mat n (Q x1 x2 x3 x4))) then True else False
+diagonal (Mat n (Q x1 x2 x3 x4)) = if(dNZero (Mat n (Q x1 x2 x3 x4))) && (uLZero (Mat n (Q x1 x2 x3 x4))) then dToList (Mat n (Q x1 x2 x3 x4)) else [] False
 
 dNZero Mat{nexp=n, mat=(C x)} = x/=0 || n==0
 dNZero (Mat n (Q x1 (C x2) (C x3) x4)) = if n>0 then ((dNZero Mat{nexp=(n-1), mat=x1}) && (dNZero Mat{nexp=(n-1), mat=x4})) else False
