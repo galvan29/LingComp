2f686a1f526db7c0fa9d593662ed0cfaabf68a16
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-16 17:16:11 +0200

    update p1.hs

--- p1.hs
+++ p1.hs
@@ -5,15 +5,23 @@ main = do
    let z = (Q (C 1) (C 0) (C 0) (C 1))
    print (diagonal (Mat 2 (Q z (C 0) (C 0) z)))
 
-diagonal (Mat n (Q x1 x2 x3 x4)) = if(diagonal3 (Mat n (Q x1 x2 x3 x4))) && (diagonal2 (Mat n (Q x1 x2 x3 x4))) then True else False
+diagonal (Mat n (Q x1 x2 x3 x4)) = if(dNZero (Mat n (Q x1 x2 x3 x4))) && (uLZero (Mat n (Q x1 x2 x3 x4))) then True else False
 
-diagonal3 Mat{nexp=n, mat=(C x)} = x/=0 || n==0
-diagonal3 (Mat n (Q x1 (C x2) (C x3) x4)) = if n>0 then ((diagonal3 Mat{nexp=(n-1), mat=x1}) && (diagonal3 Mat{nexp=(n-1), mat=x4})) else False
-diagonal3 (Mat n (Q x1 x2 x3 x4)) = if n>0 then 
-                ((diagonal3 (Mat (n-1) x1)) && (diagonal3 (Mat (n-1) x4))) else False
+dNZero Mat{nexp=n, mat=(C x)} = x/=0 || n==0
+dNZero (Mat n (Q x1 (C x2) (C x3) x4)) = if n>0 then ((dNZero Mat{nexp=(n-1), mat=x1}) && (dNZero Mat{nexp=(n-1), mat=x4})) else False
+dNZero (Mat n (Q x1 x2 x3 x4)) = if n>0 then 
+                ((dNZero (Mat (n-1) x1)) && (dNZero (Mat (n-1) x4))) else False
 
-diagonal2 Mat{nexp=n, mat=(C x)} = x==0 || n==0
-diagonal2 (Mat n (Q (C x1) x2 x3 (C x4))) = if n>0 then ((diagonal2 Mat{nexp=(n-1), mat=x2}) && (diagonal2 Mat{nexp=(n-1), mat=x3})) else False
-diagonal2 (Mat n (Q x1 x2 x3 x4)) = if n>0 then 
-                ((diagonal2 (Mat (n-1) x2)) && (diagonal2 (Mat (n-1) x3))) else False
+uLZero Mat{nexp=n, mat=(C x)} = x==0 || n==0
+uLZero (Mat n (Q (C x1) x2 x3 (C x4))) = if n>0 then ((uLZero Mat{nexp=(n-1), mat=x2}) && (uLZero Mat{nexp=(n-1), mat=x3})) else False
+uLZero (Mat n (Q x1 x2 x3 x4)) = if n>0 then 
+                ((uLZero (Mat (n-1) x2)) && (uLZero (Mat (n-1) x3))) else False
 
+dToList Mat{nexp=n, mat=(C x)} = [x]
+dToList (Mat n (Q x1 (C x2) (C x3) x4)) = if n>0 then ((dToList Mat{nexp=(n-1), mat=x1}) ++ (dToList Mat{nexp=(n-1), mat=x4}))
+dToList (Mat n (Q x1 x2 x3 x4)) = if n>0 then ((dToList (Mat (n-1) x1)) ++ (dToList (Mat (n-1) x4)))
+                
+
+
+
+{- mettere tipo sopra -}
