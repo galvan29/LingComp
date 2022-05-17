2e614460d5901096a2f555f0aa9de655ae6e7d61
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-17 18:07:42 +0200

    update 1.hs

--- qt/1.hs
+++ qt/1.hs
@@ -2,11 +2,12 @@ data QT a = C a | Q (QT a) (QT a) (QT a) (QT a) deriving (Eq, Show)
 
 main = do
    print (buildNSimplify (C 1) (C 2) (C 3) (C 4))
+   print (buildNSimplify 4 (Q (C 1) (C 2) (C 3) (C 4)))
 
 buildNSimplify :: (Eq a, Show a) => QT a -> QT a -> QT a -> QT a -> QT a
 buildNSimplify a b c d = (Q a b c d) 
 
 
-fu :: (Eq a, Show a) => Int -> QT a -> QT a
+fu :: (Eq a, Show a, Num a) => Int -> QT a -> QT a
 fu n (C x) = if(n>0) then (fu (n-1) (Q (C x) (C x) (C x) (C x))) else (C 1)
 fu n (Q a b c d) = if(n>0) then (Q (fu (n-1) a) (fu (n-1) b) c d) else (Q (C 1) (C 1) c d)
