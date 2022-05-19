db1bbf141b42c3185222a5a71bfd1a2167f36b06
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-19 12:14:47 +0200

    update dai.hs

--- dai.hs
+++ dai.hs
@@ -15,7 +15,7 @@ dNZero (Mat n (Q x1 x2 x3 x4)) = if (n>0) then ((dNZero (Mat (n-1) x1)) && (dNZe
 
 uLZero :: (Eq a, Num a) =>  Mat a -> Bool
 uLZero Mat{nexp=n, mat=(C x)} = x==0 || n==0
-uLZero (Mat n (Q x1 x2 x3 x4)) = if n>0 then ((uLZero (Mat (n-1) x1)) && (uLZero (Mat (n-1) x2)) && (uLZero (Mat (n-1) x3)) && (uLZero (Mat (n-1) x4))) else False
+uLZero (Mat n (Q x1 x2 x3 x4)) = if (n>0) then ((uLZero (Mat (n-1) x1)) && (uLZero (Mat (n-1) x2)) && (uLZero (Mat (n-1) x3)) && (uLZero (Mat (n-1) x4))) else False
 
 dToList :: Mat a -> [a]
 dToList Mat{nexp=n, mat=(C x)} = [x]
