7394172ca1b9cc8002ebbee7c1017241870cb295
galvan29 <50354804+galvan29@users.noreply.github.com>
2022-05-19 12:16:28 +0200

    update dai.hs

--- dai.hs
+++ dai.hs
@@ -7,7 +7,7 @@ main = do
    print (diagonal (Mat 2 (Q z z z z)))
    
 diagonal :: (Eq a, Num a) => Mat a -> Maybe [a]
-diagonal mat = if (dNZero mat && uLZero mat) then Just (dToList mat) else Nothing
+diagonal mat = if (dNZero mat) then Just (dToList mat) else Nothing
 
 dNZero :: (Eq a, Num a) => Mat a -> Bool
 dNZero Mat{nexp=n, mat=(C x)} = x/=0 || n==0
@@ -18,5 +18,5 @@ uLZero (Mat n (C x)) = if(x==0) then True else False
 uLZero (Mat n (Q x1 x2 x3 x4)) = if (n>0) then ((uLZero (Mat (n-1) x1)) && (uLZero (Mat (n-1) x2)) && (uLZero (Mat (n-1) x3)) && (uLZero (Mat (n-1) x4))) else False
 
 dToList :: Mat a -> [a]
-dToList (Mat n (C x))= [x]
+dToList (Mat n (C x)) = [x]
 dToList (Mat n (Q x1 x2 x3 x4)) = ((dToList (Mat (n-1) x1)) ++ (dToList (Mat (n-1) x4)))
